1ce8ee35f3add6abba73fd38c8e1ce211f8896f8
PN <p.noesel@gmx.de/usr/local/bin/python3>
2022-03-19 18:54:17 +0100

    aktuelle Version

--- NN-Test.py
+++ NN-Test.py
@@ -7,18 +7,41 @@ from sklearn.model_selection import train_test_split
 #Aufbau des NN:
 class Model(torch.nn.Module):
     def __init__(self):
-        super(mopdel, self).__init__()
+        super(Model, self).__init__()
         self.fc1 = nn.Linear(2,10)
         self.fc2 = nn.Linear(10,10)
         self.fc3 = nn.Linear(10,1)
 
     def forward(self,x):
-        x=x,to(torch.float32)
+        x=x.to(torch.float32)
         x= F.relu(self.fc1(x))
         x= F.relu(self.fc2(x))
         x= torch.sigmoid(self.fc3(x))
 
         return x
 
+
 #Datenverarbeitung:
-data = np.loadtxt ("./data/no_colums_iris_binary.csv", delimiter=",")
+data = np.loadtxt("data/no_colums_iris_binary.csv", delimiter=",")  # mit Header:,skiprows = 1
+x,y = torch.from_numpy(data[:, 0:-3]), torch.from_numpy(data[:, -1:])
+x = x.to(torch.float32)
+y = y.to(torch.float32)
+X_train, X_test, y_train, y_test =train_test_split(x,y, test_size=0.2, random_state=2)
+
+#Definitionen
+model = Model()
+loss = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+epochs = 1000
+epoch_errors =[]
+accuracy_list =[]
+print (x.size())
+print(y.size())
+#Training
+for epoch in range(epochs):
+    error = loss(model(X_train),y_train)
+    epoch_errors.append(error.item)
+    error.backward()
+    optimizer.step()
+
+print(model(torch.tensor([3,2])))
